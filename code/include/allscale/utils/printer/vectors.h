459244e45dd755561b0d98b72f7355387fa3ecb7
Herbert <herbert@dps.uibk.ac.at>
2016-03-11 16:17:16 +0100

    Fixed printers for pairs and vectors.

--- code/include/allscale/utils/printer/vectors.h
+++ code/include/allscale/utils/printer/vectors.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <utility>
+#include <vector>
 #include <iostream>
+
+#include "allscale/utils/printer/join.h"
 
 namespace std {
 
-	template<typename A,typename B>
-	ostream& operator<<(ostream& out, const pair<A,B>& data) {
-		return out << "[" << data.first << "," << data.second << "]";
+	template<typename E,typename A>
+	ostream& operator<<(ostream& out, const vector<E,A>& data) {
+		return out << "[" << allscale::utils::join(",", data) << "]";
 	}
 
 }
