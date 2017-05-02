1585880daf280cbadf589cc3c53de86f116904cd
Herbert Jordan <herbert@dps.uibk.ac.at>
2017-05-02 14:19:53 +0200

    Added static grid data item implementation.

---
+++ code/api/include/allscale/api/user/data/static_grid.h
@@ -0,0 +1,296 @@
+#pragma once
+
+#include "allscale/api/user/data/grid.h"
+
+namespace allscale {
+namespace api {
+namespace user {
+namespace data {
+
+
+	// ---------------------------------------------------------------------------------
+	//								 Declarations
+	// ---------------------------------------------------------------------------------
+
+
+	using coordinate_type = std::int64_t;
+
+	template<std::size_t Dims>
+	using StaticGridPoint = GridPoint<Dims>;
+
+	template<std::size_t Dims>
+	using StaticGridBox = GridBox<Dims>;
+
+	template<std::size_t Dims>
+	using StaticGridRegion = GridRegion<Dims>;
+
+	template<typename T, std::size_t ... Sizes>
+	class StaticGridFragment;
+
+	template<typename T, std::size_t ... Sizes>
+	class StaticGrid;
+
+
+
+
+	// ---------------------------------------------------------------------------------
+	//								  Definitions
+	// ---------------------------------------------------------------------------------
+
+
+	template<typename T, std::size_t ... Sizes>
+	class StaticGridFragment {
+	public:
+
+		enum { Dims = sizeof...(Sizes) };
+
+		using shared_data_type = core::no_shared_data;
+		using facade_type = StaticGrid<T,Sizes...>;
+		using region_type = StaticGridRegion<Dims>;
+
+	private:
+
+		using point = StaticGridPoint<Dims>;
+		using box = StaticGridBox<Dims>;
+
+		region_type size;
+
+		utils::LargeArray<T> data;
+
+	public:
+
+		StaticGridFragment(const region_type& size)
+			: StaticGridFragment(core::no_shared_data(), size) {}
+
+		StaticGridFragment(const core::no_shared_data&, const region_type& size) : size(size), data(area(totalSize())) {
+			// allocate covered data space
+			size.scanByLines([&](const point& a, const point& b) {
+				data.allocate(flatten(a),flatten(b));
+			});
+		}
+
+		T& operator[](const point& pos) {
+			return data[flatten(pos)];
+		}
+
+		const T& operator[](const point& pos) const {
+			return data[flatten(pos)];
+		}
+
+		StaticGrid<T,Sizes...> mask() {
+			return StaticGrid<T,Sizes...>(*this);
+		}
+
+		const region_type& getCoveredRegion() const {
+			return size;
+		}
+
+		point totalSize() const {
+			return point({ Sizes... });
+		}
+
+		void resize(const region_type& newSize) {
+
+			// get the difference
+			region_type plus  = region_type::difference(newSize,size);
+			region_type minus = region_type::difference(size,newSize);
+
+			// update the size
+			size = newSize;
+
+			// allocated new data
+			plus.scanByLines([&](const point& a, const point& b){
+				data.allocate(flatten(a),flatten(b));
+			});
+
+			// free excessive memory
+			minus.scanByLines([&](const point& a, const point& b){
+				data.free(flatten(a),flatten(b));
+			});
+		}
+
+		void insert(const StaticGridFragment& other, const region_type& area) {
+			assert_true(core::isSubRegion(area,other.size)) << "New data " << area << " not covered by source of size " << size << "\n";
+			assert_true(core::isSubRegion(area,size))       << "New data " << area << " not covered by target of size " << size << "\n";
+
+			// copy data line by line using memcpy
+			area.scanByLines([&](const point& a, const point& b){
+				auto start = flatten(a);
+				auto length = (flatten(b) - start) * sizeof(T);
+				std::memcpy(&data[start],&other.data[start],length);
+			});
+		}
+
+		void save(utils::Archive& /*a*/, const region_type& /*keys*/) const {
+			assert_not_implemented();
+		}
+
+		void load(utils::Archive& /*a*/) {
+			assert_not_implemented();
+		}
+
+	private:
+
+		static std::size_t area(const StaticGridPoint<Dims>& pos) {
+			std::size_t res = 1;
+			for(std::size_t i=0; i<Dims; ++i) {
+				res *= pos[i];
+			}
+			return res;
+		}
+
+		coordinate_type flatten(const StaticGridPoint<Dims>& pos) const {
+			coordinate_type res = 0;
+			coordinate_type size = 1;
+
+			for(int i=Dims-1; i>=0; i--) {
+				res += pos[i] * size;
+				size *= this->size.getTotal()[i];
+			}
+
+			return res;
+		}
+
+	};
+
+	template<typename T, std::size_t ... Sizes>
+	class StaticGrid : public core::data_item<StaticGridFragment<T,Sizes...>> {
+
+		/**
+		 * A pointer to an underlying fragment owned if used in an unmanaged state.
+		 */
+		std::unique_ptr<StaticGridFragment<T,Sizes...>> owned;
+
+		/**
+		 * A reference to the fragment instance operating on, referencing the owned fragment or an externally managed one.
+		 */
+		StaticGridFragment<T,Sizes...>* base;
+
+		/**
+		 * Enables fragments to use the private constructor below.
+		 */
+		friend class StaticGridFragment<T,Sizes...>;
+
+		/**
+		 * The constructor to be utilized by the fragment to create a facade for an existing fragment.
+		 */
+		StaticGrid(StaticGridFragment<T,Sizes...>& base) : base(&base) {}
+
+	public:
+
+		/**
+		 * The number of dimensions.
+		 */
+		enum { dimensions = sizeof...(Sizes) };
+
+		/**
+		 * The type of coordinate utilized by this type.
+		 */
+		using coordinate_type = StaticGridPoint<dimensions>;
+
+		/**
+		 * The type of region utilized by this type.
+		 */
+		using region_type = StaticGridRegion<dimensions>;
+
+		/**
+		 * Creates a new map covering the given region.
+		 */
+		StaticGrid()
+			: owned(std::make_unique<StaticGridFragment<T,Sizes...>>(region_type(size(),0,size()))), base(owned.get()) {}
+
+		/**
+		 * Disable copy construction.
+		 */
+		StaticGrid(const StaticGrid&) = delete;
+
+		/**
+		 * Enable move construction.
+		 */
+		StaticGrid(StaticGrid&&) = default;
+
+		/**
+		 * Disable copy-assignments.
+		 */
+		StaticGrid& operator=(const StaticGrid&) = delete;
+
+		/**
+		 * Enable move assignments.
+		 */
+		StaticGrid& operator=(StaticGrid&&) = default;
+
+		/**
+		 * Obtains the full size of this grid.
+		 */
+		coordinate_type size() const {
+			return coordinate_type({ Sizes ... });
+		}
+
+		/**
+		 * Provides read/write access to one of the values stored within this grid.
+		 */
+		T& operator[](const coordinate_type& index) {
+			return (*base)[index];
+		}
+
+		/**
+		 * Provides read access to one of the values stored within this grid.
+		 */
+		const T& operator[](const coordinate_type& index) const {
+			return (*base)[index];
+		}
+
+		/**
+		 * A sequential scan over all elements within this grid, providing
+		 * read-only access.
+		 */
+		template<typename Op>
+		void forEach(const Op& op) const {
+			allscale::api::user::detail::forEach(
+					coordinate_type(0),
+					size(),
+					[&](const auto& pos){
+						op((*this)[pos]);
+					}
+			);
+		}
+
+		/**
+		 * A sequential scan over all elements within this grid, providing
+		 * read/write access.
+		 */
+		template<typename Op>
+		void forEach(const Op& op) {
+			allscale::api::user::detail::forEach(
+					coordinate_type(0),
+					size(),
+					[&](const auto& pos){
+						op((*this)[pos]);
+					}
+			);
+		}
+
+		/**
+		 * A sequential scan over all elements within this grid, providing
+		 * read-only access.
+		 */
+		template<typename Op>
+		auto pforEach(const Op& op) const {
+			return pfor(size(),op);
+		}
+
+		/**
+		 * A parallel scan over all elements within this grid, providing
+		 * read/write access.
+		 */
+		template<typename Op>
+		auto pforEach(const Op& op) {
+			return pfor(size(),op);
+		}
+
+	};
+
+} // end namespace data
+} // end namespace user
+} // end namespace api
+} // end namespace allscale
---
+++ code/api/test/user/data/static_grid.cc
@@ -0,0 +1,304 @@
+#include <gtest/gtest.h>
+
+#include "allscale/api/user/data/static_grid.h"
+#include "allscale/utils/string_utils.h"
+
+#include "allscale/utils/printer/vectors.h"
+#include "allscale/utils/printer/pairs.h"
+
+namespace allscale {
+namespace api {
+namespace user {
+namespace data {
+
+	#include "data_item_test.inl"
+
+	TEST(StaticGridFragment,Basic) {
+
+		EXPECT_TRUE((core::is_fragment<StaticGridFragment<double,100,200>>::value));
+
+		StaticGridPoint<2> size = {100,200};
+
+		StaticGridRegion<2> region(size,20,30);
+		StaticGridFragment<int,100,200> fA(region);
+
+		EXPECT_EQ(size,fA.totalSize());
+	}
+
+
+	TEST(StaticGridRegion,TypeProperties) {
+
+		using namespace detail;
+
+		EXPECT_TRUE(utils::is_value<StaticGridRegion<0>>::value);
+		EXPECT_TRUE(utils::is_value<StaticGridRegion<1>>::value);
+		EXPECT_TRUE(utils::is_value<StaticGridRegion<2>>::value);
+		EXPECT_TRUE(utils::is_value<StaticGridRegion<3>>::value);
+
+		EXPECT_TRUE(utils::is_serializable<StaticGridRegion<0>>::value);
+		EXPECT_TRUE(utils::is_serializable<StaticGridRegion<1>>::value);
+		EXPECT_TRUE(utils::is_serializable<StaticGridRegion<2>>::value);
+		EXPECT_TRUE(utils::is_serializable<StaticGridRegion<3>>::value);
+
+		EXPECT_TRUE(core::is_region<StaticGridRegion<0>>::value);
+		EXPECT_TRUE(core::is_region<StaticGridRegion<1>>::value);
+		EXPECT_TRUE(core::is_region<StaticGridRegion<2>>::value);
+		EXPECT_TRUE(core::is_region<StaticGridRegion<3>>::value);
+
+	}
+
+	TEST(StaticGridFragment,TypeProperties) {
+
+		using namespace detail;
+
+		EXPECT_TRUE((core::is_fragment<StaticGridFragment<int,10>>::value));
+		EXPECT_TRUE((core::is_fragment<StaticGridFragment<int,10,20>>::value));
+		EXPECT_TRUE((core::is_fragment<StaticGridFragment<int,10,20,30>>::value));
+
+	}
+
+
+	TEST(StaticGridFragment1D,FragmentTestBasic) {
+
+		StaticGridPoint<1> size = 50;
+
+		StaticGridRegion<1> a(size,5,10);
+		StaticGridRegion<1> b(size,8,14);
+
+		testFragment<StaticGridFragment<int,50>>(a,b);
+
+	}
+
+	TEST(StaticGridFragment2D,FragmentTestBasic) {
+
+		StaticGridPoint<2> size = {50,60};
+
+		StaticGridRegion<2> a(size,{5,6},{10,12});
+		StaticGridRegion<2> b(size,{8,9},{14,16});
+
+		testFragment<StaticGridFragment<int,50,60>>(a,b);
+
+	}
+
+
+	TEST(StaticGrid,TypeProperties) {
+
+		using namespace detail;
+
+		EXPECT_TRUE((core::is_data_item<StaticGrid<int,10>>::value));
+		EXPECT_TRUE((core::is_data_item<StaticGrid<int,10,20>>::value));
+		EXPECT_TRUE((core::is_data_item<StaticGrid<int,10,20,30>>::value));
+
+	}
+
+	TEST(StaticGrid2D, Size) {
+
+		StaticGrid<int,10,20> grid;
+
+		EXPECT_EQ("[10,20]",toString(grid.size()));
+
+
+	}
+
+	struct InstanceCounted {
+
+		static int num_instances;
+
+		InstanceCounted() {
+			num_instances++;
+		}
+
+		~InstanceCounted() {
+			num_instances--;
+		}
+
+	};
+
+	int InstanceCounted::num_instances = 0;
+
+
+	TEST(StaticGrid2D, ElementCtorAndDtor) {
+
+
+		// ----- test the Instance Counted class --------
+
+		// start with assuming that there are no instances
+		EXPECT_EQ(0,InstanceCounted::num_instances);
+
+		// check that the constructor works as expected
+		{
+			InstanceCounted a;
+			EXPECT_EQ(1,InstanceCounted::num_instances);
+		}
+
+		// also check that the destructor works as expected
+		EXPECT_EQ(0,InstanceCounted::num_instances);
+
+
+		// ---------- test the Large Array ---------------
+
+		{
+
+			// create a large array
+			StaticGrid<InstanceCounted,10,20> a;
+
+			// now there should be a lot of instances
+			EXPECT_EQ(200,InstanceCounted::num_instances);
+
+			// here they should get destroyed
+		}
+
+		// check that there are none left
+		EXPECT_EQ(0,InstanceCounted::num_instances);
+	}
+
+	TEST(StaticGrid2D, ComplexDataStructureCtorDtor) {
+
+		// create a large array
+		StaticGrid<std::vector<int>,10,20> a;
+
+		// initialize each value
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				a[{i,j}].push_back(i*j);
+			}
+		}
+	}
+
+	TEST(StaticGrid2D, Move) {
+
+		// create a large array
+		StaticGrid<std::vector<int>,10,20> a;
+
+		// initialize each value
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				a[{i,j}].push_back(i*j);
+			}
+		}
+
+		// move the data to a new instance
+		StaticGrid<std::vector<int>,10,20> b(std::move(a));
+
+		// check the content
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				EXPECT_EQ(1,(b[{i,j}].size()));
+				EXPECT_EQ(i*j,(b[{i,j}].front()));
+			}
+		}
+
+		// now move-assign the values
+		a = std::move(b);
+
+		// check the content
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				EXPECT_EQ(1,(a[{i,j}].size()));
+				EXPECT_EQ(i*j,(a[{i,j}].front()));
+			}
+		}
+	}
+
+	TEST(StaticGrid2D,ExampleManagement) {
+
+		using Point = StaticGridPoint<2>;
+		using Region = StaticGridRegion<2>;
+		using Fragment = StaticGridFragment<int,500,1000>;
+		using SharedData = core::no_shared_data;
+
+		SharedData shared;
+
+		// total size:
+		Point size = { 500, 1000 };
+
+		// upper half
+		Region partA(size, {0,0},{250,1000});
+
+		// lower half
+		Region partB(size, {250,0},{500,1000});
+
+		// check that the coordinates are correct
+		Region full = Region::merge(partA,partB);
+		EXPECT_EQ("{[[0,0] - [500,1000]]}",toString(full));
+
+		// create fragments
+		Fragment fA(shared,partA);
+		Fragment fB(shared,partB);
+
+		// initialize
+		fA.mask().forEach([](auto& p) { p = 0; });
+		fB.mask().forEach([](auto& p) { p = 0; });
+
+		// fill the data set
+		for(int t = 1; t<10; t++) {
+
+			auto a = fA.mask();
+			for(unsigned i=0; i<250; i++) {
+				for(unsigned j=0; j<1000; j++) {
+					EXPECT_EQ((i*j*(t-1)),(a[{i,j}]));
+					a[{i,j}] = i * j * t;
+				}
+			}
+
+			auto b = fB.mask();
+			for(unsigned i=250; i<500; i++) {
+				for(unsigned j=0; j<1000; j++) {
+					EXPECT_EQ((i*j*(t-1)),(b[{i,j}]));
+					b[{i,j}] = i * j * t;
+				}
+			}
+
+		}
+
+		// --- alter data distribution ---
+
+
+		Region newPartA = Region(size, {0,0}, {250,750});
+		Region newPartB = Region(size, {250,0}, {500,750});
+		Region newPartC = Region(size, {0,750}, {500,1000});
+		EXPECT_EQ(full,Region::merge(newPartA,newPartB,newPartC));
+
+		Fragment fC(shared,newPartC);
+
+		// move data from A and B to C
+		fC.insert(fA,Region::intersect(newPartC,partA));
+		fC.insert(fB,Region::intersect(newPartC,partB));
+
+		// shrink A and B
+		fA.resize(newPartA);
+		fB.resize(newPartB);
+
+		for(int t = 10; t<20; t++) {
+
+			auto a = fA.mask();
+			for(unsigned i=0; i<250; i++) {
+				for(unsigned j=0; j<750; j++) {
+					EXPECT_EQ((i*j*(t-1)),(a[{i,j}]));
+					a[{i,j}] = i * j * t;
+				}
+			}
+
+			auto b = fB.mask();
+			for(unsigned i=250; i<500; i++) {
+				for(unsigned j=0; j<750; j++) {
+					EXPECT_EQ((i*j*(t-1)),(b[{i,j}]));
+					b[{i,j}] = i * j * t;
+				}
+			}
+
+			auto c = fC.mask();
+			for(unsigned i=0; i<500; i++) {
+				for(unsigned j=750; j<1000; j++) {
+					EXPECT_EQ((i*j*(t-1)),(c[{i,j}]));
+					c[{i,j}] = i * j * t;
+				}
+			}
+		}
+
+	}
+
+} // end namespace data
+} // end namespace user
+} // end namespace api
+} // end namespace allscale
