7d0da57fff44affeb85d355bd9080b2ef54e1ee0
Philipp Gschwandtner <philipp@dps.uibk.ac.at>
2017-06-01 17:41:05 +0200

    fixed an MSVC conversion warning and constexpr compiler error

--- code/tutorials/src/heat_stencil/06_stencil.cxx
+++ code/tutorials/src/heat_stencil/06_stencil.cxx
@@ -16,7 +16,7 @@ int main() {
 	const double k = 0.001;
 
 	using Grid = data::StaticGrid<double,N,N>;
-	using Point = allscale::utils::Vector<int,2>;
+	using Point = allscale::utils::Vector<std::size_t,2>;
 
 	Grid temp;
 
