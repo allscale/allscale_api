cae7cd6f35940bd0f5871b5dd507e08e5e17c34a
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2017-06-21 10:51:38 +0200

    CI: Add CI scripts

---
+++ scripts/ci/defaults.sh
@@ -0,0 +1,25 @@
+# CMake Build Type (Debug / Release).
+export BUILD_TYPE="${BUILD_TYPE:-Release}"
+
+# Number of cores used for compilation and testing.
+export NPROC="${NPROC:-$(nproc)}"
+
+# Set default nice level.
+export NICE_LEVEL="${NICE_LEVEL:-10}"
+
+# Location of Third Party Libraries.
+export THIRD_PARTY_LIBS="${THIRD_PARTY_LIBS:-$HOME/third_party_libs}"
+
+# Assume Workspace if not set.
+CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export WORKSPACE="${WORKSPACE:-"$(realpath "$CI_DIR/../..")"}"
+
+# Assume Build Directory if not set.
+export BUILD_DIR="${BUILD_DIR:-$WORKSPACE/build}"
+
+# Are we running on our CI server?
+if [[ "$(hostname)" == "hudson.dps.uibk.ac.at" ]]; then
+	export RUNNING_ON_CI_SERVER="1"
+else
+	export RUNNING_ON_CI_SERVER=""
+fi
