a55b64f7a9b9d69d6b7b005ba3b65ae1f14439b3
Philipp Gschwandtner <philipp@dps.uibk.ac.at>
2017-07-17 15:25:40 +0200

    added static grid container

---
+++ code/utils/include/allscale/utils/static_grid.h
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <cstring>
+#include <type_traits>
+
+#include "allscale/utils/serializer.h"
+#include "allscale/utils/vector.h"
+
+namespace allscale {
+namespace utils {
+
+	template<typename Cell, size_t ... size>
+	struct StaticGrid;
+
+	template<typename Cell, size_t a, size_t ... rest>
+	struct StaticGrid<Cell,a,rest...> {
+		using data_type = std::array<StaticGrid<Cell,rest...>,a>;
+		using addr_type = utils::Vector<int64_t,sizeof...(rest)+1>;
+		data_type data;
+
+		StaticGrid& operator=(const StaticGrid& other) {
+			if (this == &other) return *this;
+			assignInternal<Cell>(other);
+			return *this;
+		}
+
+	private:
+
+		template<typename T>
+		typename std::enable_if<std::is_trivially_copyable<T>::value,void>::type
+		assignInternal(const StaticGrid& other) {
+			std::memcpy(&data,&other.data,sizeof(data_type));
+		}
+
+		template<typename T>
+		typename std::enable_if<!std::is_trivially_copyable<T>::value,void>::type
+		assignInternal(const StaticGrid& other) {
+			data = other.data;
+		}
+
+	public:
+
+		Cell& operator[](const addr_type& addr) {
+			std::array<int64_t, sizeof...(rest)+1> index = addr;
+			std::array<int64_t, sizeof...(rest)> nested;
+			std::copy(index.rbegin(), index.rend()-1, nested.begin());
+			return data[index.front()][nested];
+		}
+
+		const Cell& operator[](const addr_type& addr) const {
+			std::array<int64_t, sizeof...(rest)+1> index = addr;
+			std::array<int64_t, sizeof...(rest)> nested;
+			std::copy(index.rbegin(), index.rend()-1, nested.begin());
+			return data[index.front()][nested];
+		}
+
+		utils::Vector<std::size_t, sizeof...(rest)+1> size() const {
+			return { a, rest... };
+		}
+
+		template<typename Lambda>
+		void forEach(const Lambda& lambda) const {
+			for(const auto& cur : data) {
+				cur.forEach(lambda);
+			}
+		}
+
+		template<typename Lambda>
+		void forEach(const Lambda& lambda) {
+			for(auto& cur : data) {
+				cur.forEach(lambda);
+			}
+		}
+
+		void store(utils::ArchiveWriter& writer) const {
+			for(const auto& e : data) {
+				writer.write(e);
+			}
+		}
+
+		static StaticGrid load(utils::ArchiveReader& reader) {
+			StaticGrid grid;
+			for(auto& e : grid.data) {
+				e = reader.read<typename data_type::value_type>();
+			}
+			return grid;
+		}
+
+	};
+
+	template<typename Cell>
+	struct StaticGrid<Cell> {
+		using data_type = Cell;
+		using addr_type = utils::Vector<int64_t, 0>;
+
+		data_type data;
+
+		StaticGrid& operator=(const StaticGrid& other) {
+			if (this == &other) return *this;
+			assignInternal<Cell>(other);
+			return *this;
+		}
+
+	private:
+
+		template<typename T>
+		typename std::enable_if<std::is_trivially_copyable<T>::value,void>::type
+		assignInternal(const StaticGrid& other) {
+			std::memcpy(&data,&other.data,sizeof(data_type));
+		}
+
+		template<typename T>
+		typename std::enable_if<!std::is_trivially_copyable<T>::value,void>::type
+		assignInternal(const StaticGrid& other) {
+			data = other.data;
+		}
+
+	public:
+
+		Cell& operator[](const addr_type&) {
+			return data;
+		}
+
+		const Cell& operator[](const addr_type&) const {
+			return data;
+		}
+
+		std::size_t size() const {
+			return 1;
+		}
+
+		template<typename Lambda>
+		void forEach(const Lambda& lambda) const {
+			lambda(data);
+		}
+
+		template<typename Lambda>
+		void forEach(const Lambda& lambda) {
+			lambda(data);
+		}
+
+		void store(utils::ArchiveWriter& writer) const {
+			writer.write(data);
+		}
+
+		static StaticGrid load(utils::ArchiveReader& reader) {
+			StaticGrid grid;
+			grid.data = reader.read<data_type>();
+			return grid;
+		}
+
+	};
+
+} // end utils
+} // end namespace allscale
---
+++ code/utils/test/static_grid.cc
@@ -0,0 +1,108 @@
+#include <gtest/gtest.h>
+
+#include "allscale/utils/static_grid.h"
+#include "allscale/utils/serializer.h"
+
+namespace allscale {
+namespace utils {
+
+	TEST(StaticGrid1D, TypeProperties) {
+
+		using GridType = StaticGrid<int, 1>;
+
+		EXPECT_TRUE(std::is_default_constructible<GridType>::value);
+		EXPECT_TRUE(std::is_trivially_default_constructible<GridType>::value);
+
+		EXPECT_TRUE(std::is_copy_constructible<GridType>::value);
+		EXPECT_TRUE(std::is_move_constructible<GridType>::value);
+
+		EXPECT_TRUE(std::is_trivially_copy_constructible<GridType>::value);
+		EXPECT_TRUE(std::is_trivially_move_constructible<GridType>::value);
+
+		EXPECT_TRUE(std::is_copy_assignable<GridType>::value);
+		EXPECT_TRUE(std::is_move_assignable<GridType>::value);
+
+		EXPECT_TRUE((utils::is_serializable<GridType>::value));
+
+	}
+
+	TEST(StaticGrid1D, Basic) {
+
+		StaticGrid<int, 1> grid;
+
+		EXPECT_EQ((Vector<size_t, 1>{1}), grid.size());
+
+		grid[{0}] = 42;
+		EXPECT_EQ(42, grid[{0}]);
+
+	}
+
+	TEST(StaticGrid2D, Basic) {
+
+		StaticGrid<int, 3, 5> grid;
+
+		EXPECT_EQ((Vector<size_t, 2>{3,5}), grid.size());
+
+		grid.forEach([](int& element) {
+			element = 42;
+		});
+		
+		grid.forEach([](const int& element) {
+			EXPECT_EQ(42, element);
+		});
+
+	}
+	
+	TEST(StaticGrid2D, Indexing) {
+
+		StaticGrid<int, 3, 5> grid;
+
+		EXPECT_EQ((Vector<size_t, 2>{3, 5}), grid.size());
+
+		int count = 0;
+		for(size_t i = 0; i < grid.size().x; ++i) {
+			for(size_t j = 0; j < grid.size().y; ++j) {
+				grid[{i, j}] = count++;
+			}
+		}
+	
+		count = 0;
+		grid.forEach([&count](const int& element) {
+			EXPECT_EQ(count++, element);
+		});
+
+	}
+
+	TEST(StaticGrid2D, Addressing) {
+
+		StaticGrid<int, 3, 5> grid;
+
+		grid[{0, 0}] = 2;
+		EXPECT_EQ(2, (grid[{0, 0}]));
+
+		Vector<size_t, 2> index{ 0,0 };
+		EXPECT_EQ(2, (grid[StaticGrid<int, 3, 5>::addr_type({ 0,0 })]));
+	}
+
+	TEST(StaticGrid2D, Serialization) {
+
+		StaticGrid<int, 3, 5> grid;
+
+		int count = 0;
+		grid.forEach([&count](int& element) {
+			element = count++;
+		});
+
+		utils::Archive archive = utils::serialize(grid);
+
+		StaticGrid<int, 3, 5> newGrid = utils::deserialize<StaticGrid<int, 3, 5>>(archive);
+
+		count = 0;
+		newGrid.forEach([&count](const int& element) {
+			EXPECT_EQ(count++, element);
+		});
+
+	}
+
+} // end namespace utils
+} // end namespace allscale
