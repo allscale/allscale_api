03014cd4440d1589535168a31c709675f6066c78
Herbert Jordan <herbert@dps.uibk.ac.at>
2017-11-09 11:15:47 +0100

    Refactored plotting.

--- code/tutorials/src/adaptivegrid/00_wave_simulation.cxx
+++ code/tutorials/src/adaptivegrid/00_wave_simulation.cxx
@@ -16,15 +16,15 @@ using Sigma = allscale::utils::Vector<double, 2>;
 // -- a generic update function for the init and update step --
 
 struct init_config {
-	static constexpr double a = .5;
-	static constexpr double b =  0;
-	static constexpr double c = .5;
+    static constexpr double a = .5;
+    static constexpr double b =  0;
+    static constexpr double c = .5;
 };
 
 struct update_config {
-	static constexpr double a = 1;
-	static constexpr double b = 1;
-	static constexpr double c = 1;
+    static constexpr double a = 1;
+    static constexpr double b = 1;
+    static constexpr double c = 1;
 };
 
 template<typename config>
@@ -50,11 +50,11 @@ void step(Grid& up, const Grid& u, const Grid& um, double dt, const Delta delta)
 // -- init and update wrappers --
 
 void initialize(Grid& up, const Grid& u, const Grid& um, double dt, const Delta delta) {
-	step<init_config>(up,u,um,dt,delta);
+    step<init_config>(up,u,um,dt,delta);
 }
 
 void update(Grid& up, const Grid& u, const Grid& um, double dt, const Delta delta) {
-	step<update_config>(up,u,um,dt,delta);
+    step<update_config>(up,u,um,dt,delta);
 }
 
 
@@ -72,14 +72,56 @@ void setupWave(Grid& u, const Point& center, double amp, const Sigma s) {
 }
 
 
+// a utility to compute the volume, which must be constant over the simulation
+double volume(const Grid& u) {
+    auto size = u.size();
+    double sum = 0;
+    for(int i=0; i<size.x;i++) {
+        for(int j=0; j<size.y; j++) {
+            sum += u[{i,j}];
+        }
+    }
+    return sum;
+}
+
+// a utility to plot the current state of the adaptive mesh
+void plot(const Grid& u) {
+
+    // scale the plot down to may 51 x 51
+    auto size = u.size();
+    int scale = std::max(size.x/51,size.y/51);
+
+    double sum = 0;
+    for(int i=0; i<size.x;i+=scale) {
+        // plot value of the surface
+        for(int j=0; j<size.y; j+=scale) {
+            auto v = u[{i,j}];
+            sum += v;
+            std::cout << (
+                    (v >  0.3) ? 'X' :
+                    (v >  0.1) ? '+' :
+                    (v > -0.1) ? '-' :
+                    (v > -0.3) ? '.' :
+                                 ' '
+            );
+        }
+        std::cout << "\n";
+    }
+    std::cout << "Volume: " << sum << "\n";
+    std::cout << "\n";
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+}
+
+
 // -- the actual simulation --
 
 int main() {
 
     // -- simulation parameters --
 
-    const int N = 51;
-    const double T = 600;
+    const int N = 201;
+    const double T = 400;
 
     const double dt = 0.25;
     const double dx = 2;
@@ -109,34 +151,26 @@ int main() {
     // -- simulation --
 
     //simulate time steps
+    double vol_0,vol_1;
     for(double t =0; t<=T; t+=dt) {
         std::cout << "t=" << t << "\n";
+
+        // get volume before
+        vol_0 = volume(u);
 
         //simulate one simulation step
         update(up, u, um, dt, {dx,dy});
 
-        // print out current state
-        {
-            double sum = 0;
-            for(int i=0; i<rows;i++) {
-                for(int j=0; j<columns; j++) {
-                    auto v = u[{i,j}];
-                    sum += v;
-                    std::cout << (
-                            (v >  0.3) ? 'X' :
-                            (v >  0.1) ? '+' :
-                            (v > -0.1) ? '-' :
-                            (v > -0.3) ? '.' :
-                                         ' '
-                    );
-                }
-                std::cout << "\n";
-            }
-            std::cout << "Volume: " << sum << "\n";
-            std::cout << "\n";
+        // get volume after
+        vol_1 = volume(up);
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        }
+        // print out current state
+        plot(up);
+
+        // check that volume remained constant
+        assert_lt(fabs(vol_0-vol_1),0.01)
+            << "Before: " << vol_0 << "\n"
+            << "After:  " << vol_1;
 
         // rotate grids
         std::swap(um, u);
